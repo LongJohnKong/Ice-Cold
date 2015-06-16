d67ed626319e01af6f8ab11f235f5ea94ffd7757
MarounR <rizk74@hotmail.com>
2015-06-16 21:18:02 +1000

    SUCK MY LEFT NUT

--- demo.js
+++ demo.js
@@ -1,13 +1,35 @@
 //DEMO
 
 var player = new Player();
+var Grass = document.createElement("img");
 
+Grass.src = "grass.png";
+
+var Background = [];
+
+for (var y = 0; y < 15; y++)
+{
+	Background[y] = [];
+	for (var x = 0; x < 20; x++)
+	{
+		Background[y][x] = Grass;
+	}
+}
 function DEMONSTRATION(deltatime)
 {
 
 	console.log("DEMO LOADED")
 	context.fillStyle = "#ccc";
 	context.fillRect(0, 0, canvas.width, canvas.height);
+		for (var y = 0; y < 15; y++)
+	{
+		for (var x = 0; x < 20; x++)
+		{
+			context.drawImage( Background[y][x],
+							   x * 32, y * 32);
+		}
+	}
 	player.draw(context)
 	player.update(deltatime)
 }
+
