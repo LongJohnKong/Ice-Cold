6f6f85626c48366e914f4cbc814b4697f30d41ff
MarounR <rizk74@hotmail.com>
2015-06-23 18:16:58 +1000

    Draws Background

--- demo.js
+++ demo.js
@@ -3,11 +3,31 @@
 var player = new Player(640,480);
 var enemy = new Enemy(640,480);
 //var mouse = new Mouse();
+var Grass = document.createElement("img");
 
+Grass.src = "snow.png";
 
+var Background = [];
+
+for (var y = 0; y < 15; y++)
+{
+	Background[y] = [];
+	for (var x = 0; x < 20; x++)
+	{
+		Background[y][x] = Grass;
+	}
+}
 function DEMONSTRATION(deltatime)
 {
-	
+
+for (var y = 0; y < 15; y++)
+	{
+		for (var x = 0; x < 20; x++)
+		{
+			context.drawImage( Background[y][x],
+							   x * 32, y * 32);
+		}
+	}
 	console.log("DEMO LOADED")
 	context.fillStyle = "#000000"
 	context.font = "18px Arial"
