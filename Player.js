b45de80b49668b9cae1c4035a4d79133015759f9
MarounR <rizk74@hotmail.com>
2015-06-24 17:23:23 +1000

    Fixed movement

--- Player.js
+++ Player.js
@@ -14,12 +14,12 @@ var Player = function(x,y)
 	this.PositionX = x/2;
 	this.PositionY = y/2;
 	this.speed = 1;
-	this.friction  = 1;
 	this.currentSpeed = 0;
-	this.maxSpeed = 1;
+	this.minSpeed = 5;
+	this.maxSpeed = -5;
 }
 
-Player.prototype.update = function(dt)
+Player.prototype.update = function(deltaTime)
 {	
 	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true)
 	{
@@ -33,39 +33,23 @@ Player.prototype.update = function(dt)
 	
 	if(keyboard.isKeyDown(keyboard.KEY_UP) == true)
 	{
-		this.currentSpeed -= this.speed;
+		this.currentSpeed = this.maxSpeed;
 	}
 	
 	if(keyboard.isKeyDown(keyboard.KEY_DOWN) == true)
 	{
-		this.currentSpeed += this.speed;
+		this.currentSpeed = this.minSpeed;
 	}
 	
-	//Slows down the player
-	if (this.currentSpeed > 0)
+	if(keyboard.isKeyDown(keyboard.KEY_UP) == false)
 	{
-		this.currentSpeed - 1;	
+		this.currentSpeed = 0;
 	}
 	
-	if (this.currentSpeed < 0)
+	if(keyboard.isKeyDown(keyboard.KEY_DOWN) == false)
 	{
-		this.currentSpeed + 1;
-	}
-	var now = Date.now();
-	var lastUpdate = Date.now();
-	var DeltaTime = (now - this.lastUpdate) * 0.001;
-	this.lastUpdate = now;
-	
-	////Caps the player speed
-	//if (this.currentSpeed = 2)
-	//{
-	//	this.currentSpeed = 0;	
-	//}
-	//
-	//if (this.currentSpeed = -1)
-	//{
-	//	this.currentSpeed = 0;
-	//}
+		this.currentSpeed = 0;
+	}	
 	
 	//Rotation
 	var s = Math.sin(this.rotation);
@@ -80,6 +64,11 @@ Player.prototype.update = function(dt)
 	//adds our calculated velocity
 	this.PositionX += xVel;
 	this.PositionY += yVel;
+	
+	var now = Date.now();
+	var lastUpdate = Date.now();
+	var DeltaTime = (now - this.lastUpdate) * 0.001;
+	this.lastUpdate = now;
 	
 	//Rotates
 	//this.rotation += 0.1;// this.angularDirection * this.speed;	
