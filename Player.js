0f4288e1e265fdadfd9aa9074956494d53c680fe
Maroun Rizk <rizk74@hotmail.com>
2015-06-30 19:30:19 +1000

    WE GOT COLLISION

--- Player.js
+++ Player.js
@@ -55,7 +55,25 @@ Player.prototype.update = function(deltaTime)
 		}
 	}
 	
-
+	if (player.PositionX < 128 )
+		{
+		player.PositionX = 128;
+		}; 
+	if (player.PositionX > CANVASWIDTH - 128)
+	{
+	player.PositionX = CANVASWIDTH - 128;
+	}
+	
+	if (player.PositionY < 64*3 )
+	{
+	player.PositionY = 64*3;
+	}; 
+		
+	if (player.PositionY > CANVASHEIGHT - 128)
+	{
+	player.PositionY = CANVASHEIGHT - 128;
+	}
+	
 	collision.update(deltaTime);
 	
 	//Rotation
